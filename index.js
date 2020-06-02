dae23cfd2c001322b0c0fb3f5de85ab2a7c40bce
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-06-02 19:48:26 +0200

    Handle POST requests (create a link)

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const connection = require('./connection');
 
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 console.log('request was sent');
 
@@ -20,14 +21,37 @@ app.get('/api/links', (req, res) => {
     res.json(results) //res.status(200).json(results)
     console.log(results); // inutile ce console log maintenant non ?
   });
-
 })
+
+app.post('/api/links', (req, res) => {
+
+  const formData = req.body;
+
+  connection.query('INSERT INTO link SET ?', [formData], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: err.message,
+        sql: err.sql
+      });
+    }
+
+    // SELECT here
+
+    const { insertId } = results;
+    // plutôt privilégier un nouveau SELECT
+    const insertedLink = { ...formData, id: insertId };
+    res.status(201).json(insertedLink);
+  });
+});
+
+
+
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
     throw new Error('y\'a une error')
   }
-  console.log(`mon amour pour Francois est over ${process.env.PORT}`)
+  console.log(`bisous body parser ${process.env.PORT}`)
 })
 
 
